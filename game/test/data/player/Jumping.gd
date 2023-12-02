0e652d4efe8d6dbe76afe6a3ef3f36eaca63dee4
ae0nic <bzminecraft@gmail.com>
2023-12-01 20:01:19 -0600

    More checks to wall running +Adjusted wall running speed +Added effects to wall_running_angle_threshold, wall_running_angle_zone, wall_running_speed_threshold

--- game/test/data/player/Jumping.gd
+++ game/test/data/player/Jumping.gd
@@ -11,6 +11,10 @@ var jump_velocity : float
 var speed : float
 var air_resistance : float
 
+var wall_running_angle_threshold : float
+var wall_running_angle_zone : float
+var walL_running_speed_threshold : float
+
 func _ready():
 	player_variables = get_node("/root/PlayerVariables")
 	player = player_variables.player
@@ -20,6 +24,10 @@ func _ready():
 	jump_velocity = player_variables.jump_velocity
 	speed = player_variables.jumping_speed
 	air_resistance = player_variables.air_resistance
+	
+	wall_running_angle_threshold = player_variables.wall_running_angle_threshold
+	wall_running_angle_zone = player_variables.wall_running_angle_zone
+	walL_running_speed_threshold = player_variables.wall_running_speed_threshold
 
 func process_state(delta):
 	pass
@@ -31,7 +39,21 @@ func physics_process_state(delta):
 		player.add_velocity(gravity_direction * gravity * delta)
 		
 	if (player.is_on_wall_only()):
-		player.change_state(player.possible_states["Wall Running"])
+		var wall_zone_forward_threshold = player.get_wall_normal().rotated(player.up_direction, wall_running_angle_threshold)
+		var wall_zone_backward_threshold = player.get_wall_normal().rotated(player.up_direction, -wall_running_angle_threshold)
+		var player_forward_direction = Vector3.FORWARD.rotated(player.up_direction, head.rotation.y)
+		var player_angle_to_wall = min(player_forward_direction.angle_to(wall_zone_forward_threshold), player_forward_direction.angle_to(wall_zone_backward_threshold))
+		
+		var wall_relative_speed = max(wall_zone_forward_threshold.dot(player_forward_direction * player.velocity.length()), wall_zone_backward_threshold.dot(player_forward_direction * player.velocity.length()))
+		
+		print(wall_relative_speed)
+		
+		if ((player_angle_to_wall <= wall_running_angle_zone) and \
+			 wall_relative_speed >= walL_running_speed_threshold):
+				player.change_state(player.possible_states["Wall Running"])
+				print("Player is now wall running")
+			
+		
 		
 	if (player.velocity.y < 0):
 		player.change_state(player.possible_states["Falling"])
