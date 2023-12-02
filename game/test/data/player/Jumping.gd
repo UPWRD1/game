70f33aaa7fe950267f83762fd244d5b5d890e4f5
ae0nic <bzminecraft@gmail.com>
2023-12-01 18:56:10 -0600

    Wall running state +Added wall running state 	~ State is switched to while jumping and player is jumping and 	touching the wall
    	~ Player will move along the wall in the direction they are
    	looking
    
    +Abstracted most variables into player_variables.gd
    	~These variables can be used from all scripts
    
    +Added extra movement options for midair
    
    +Added multiple variables for wall running
    	-Most of these variables do not do anything at the moment
    
    ~Made floor platforms square (were rectangular before)
    
    +Adjusted gravity settings for more faithful movement

--- game/test/data/player/Jumping.gd
+++ game/test/data/player/Jumping.gd
@@ -1,25 +1,42 @@
 extends Node
 class_name JumpingState
 
+var player_variables
+
 var player : CharacterBody3D
 var gravity_direction : Vector3
 var gravity : float
-
-var jump_velocity : float = 20
+var head : Node3D
+var jump_velocity : float
+var speed : float
+var air_resistance : float
 
 func _ready():
-	player = get_node("../../") as CharacterBody3D
-	gravity_direction = ProjectSettings.get_setting("physics/3d/default_gravity_vector") as Vector3
-	gravity = ProjectSettings.get_setting("physics/3d/default_gravity") as float
+	player_variables = get_node("/root/PlayerVariables")
+	player = player_variables.player
+	gravity_direction = player_variables.gravity_direction
+	gravity = player_variables.gravity
+	head = player_variables.player.get_node("head")
+	jump_velocity = player_variables.jump_velocity
+	speed = player_variables.jumping_speed
+	air_resistance = player_variables.air_resistance
 
-func process_state(_delta):
+func process_state(delta):
 	pass
 	
 func physics_process_state(delta):
 	if (player.is_on_floor()):
 		player.add_velocity(-gravity_direction * jump_velocity)
 	else:
-		player.add_velocity(gravity_direction * gravity)
+		player.add_velocity(gravity_direction * gravity * delta)
+		
+	if (player.is_on_wall_only()):
+		player.change_state(player.possible_states["Wall Running"])
 		
 	if (player.velocity.y < 0):
 		player.change_state(player.possible_states["Falling"])
+		
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	player.add_velocity(Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, head.rotation.y) * speed * delta)
+	player.velocity.x *= air_resistance
+	player.velocity.z *= air_resistance
